a63fe561a0d9b944bfeef1c26788e4fb2993bf82
lalluprakash1995 <lalluprakash.sics@gmail.com>
2018-11-20 14:02:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lallu/lallu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lallu.lallu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
